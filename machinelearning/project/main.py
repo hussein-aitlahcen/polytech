eb14a1f2fc628feffdb95e1e7d0eeae4f5d81200
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-03-08 16:25:46 +0100

    add: license

--- machinelearning/project/main.py
+++ machinelearning/project/main.py
@@ -1,3 +1,22 @@
+# main.py ---
+
+# Copyright (C) 2018 Hussein Ait-Lahcen
+
+# Author: Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 from math import log
 from itertools import groupby
@@ -30,13 +49,13 @@ right    = lambda f, t: bimap(identity, f, t)
   [X] = list of images
   [y] = expected classes for [X]
   f = zip
-  g = groupby . sorted
-  [y], [X] -f-> [y, X] -g-> [y, [X]] -> [gi(mean([X]), cov([X]))]
+  g = groupby fst . sortby fst
+  [y], [X] -f-> [(y, X)] -g-> [(y, [X])] -> [gi(mean([X]), cov([X]))]
 '
 learn    = lambda X, y: [gi(np.mean(subX, axis=0), np.cov(subX.T)) for (y, subX) in map(partial(right, lambda x: np.array(list(map(snd, x)))), groupby(sorted(zip(y, X), key=fst), fst))]
 '
   f = enumerate
-  [X] -f-> [(i, X)] -> (y[i], max(gi(X)))
+  [X] -f-> [(i, X)] -> [(y[i], max(gi(X)))]
 '
 judge    = lambda X, y: [(y[fst(ix)], max(map(partial(right, partial(flip, app, snd(ix))), enumerate(G)), key=snd)) for ix in enumerate(X)]
 

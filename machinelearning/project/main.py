d6657dd1683de5019c09a1a7fd0461fb9112569a
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-03-08 16:54:59 +0100

    docs: more comments

--- machinelearning/project/main.py
+++ machinelearning/project/main.py
@@ -33,28 +33,39 @@ def gi(mu, C):
 
 identity = lambda x: x
 
-# left/right projection of product type
-# fst: (a x b) -> a
-#         t    -> a
-# snd: (a x b) -> b
-#         t    -> b
+''' left/right projection of product type
+
+ fst: (a x b) -> a
+         t    -> a
+ snd: (a x b) -> b
+         t    -> b
+
+'''
 fst      = lambda t: t[0]
 snd      = lambda t: t[1]
 
-# function evaluation, useful for partial application
-# (a -> b) -> a -> b
-#     f    -> x -> y
-# e.g. fix x and apply f, g, h...
+''' function evaluation, useful for partial application
+
+ (a -> b) -> a -> b
+     f    -> x -> y
+
+'''
 app      = lambda f, x: f(x)
 
-# swap function arguments
-# (a -> b -> c) -> b -> a -> c
-#       f          x -> y -> z
+''' swap function arguments
+
+ (a -> b -> c) -> b -> a -> c
+       f       -> x -> y -> z
+
+'''
 flip     = lambda f, x, y: f(y, x)
 
-# project and reconstruct product type with given functions
-# (a -> c) -> (b -> d) -> (a x b) -> (c x d)
-#    f     ->    g     ->    t    ->    u
+''' project and reconstruct product type with given functions
+
+ (a -> c) -> (b -> d) -> (a x b) -> (c x d)
+    f     ->    g     ->    t    ->    u
+
+'''
 bimap    = lambda f, g, t: (f(fst(t)), g(snd(t)))
 
 # obvious helpers for bimap
